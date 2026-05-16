2cf61337b2b221a0f21eee78534455d9b9fa93f3
sumjitg <photographyworld93@gmail.com>
2026-05-17 03:12:19 +0530

    chore: integrate nitro plugin and update vercel output configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import { tanstackStart } from "@tanstack/react-start/plugin/vite"
 import { defineConfig } from "vite"
 import viteReact from "@vitejs/plugin-react"
+import { nitro } from "nitro/vite"
 import tailwindcss from "@tailwindcss/vite"
 import tsConfigPaths from "vite-tsconfig-paths"
 
 export default defineConfig({
   plugins: [
     tanstackStart(),
+    nitro(),
     viteReact(),
     tailwindcss(),
     tsConfigPaths({ projects: ["./tsconfig.json"] }),
